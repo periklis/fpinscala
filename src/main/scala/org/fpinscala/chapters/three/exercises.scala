a6e75481f87d15f5635f686aa223da083b4a13ae
Periklis Tsirakidis <periklis.tsirakidis@holidaycheck.com>
2017-12-05 20:15:31 +0100

    Implement solutions and tests for chapter three

--- src/main/scala/org/fpinscala/chapters/three/exercises.scala
+++ src/main/scala/org/fpinscala/chapters/three/exercises.scala
@@ -39,48 +39,214 @@ object Exercises {
       case x :: xs => f(x, foldRight(xs, z)(f))
     }
 
-  def sum(as: List[Int]): Int =
-    foldRight(as, 0)(_ + _)
-
-  def product(as: List[Double]): Double =
-    foldRight(as, 1.0)(_ * _)
-
-  def length[A](as: List[A]): Int =
-    foldRight(as, 0)((_, z) => z + 1)
-
   def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B =
     as match {
       case Nil     => z
       case x :: xs => foldLeft(xs, f(z, x))(f)
     }
 
-  def sumlf(as: List[Int]): Int =
-    foldLeft(as, 0)(_ + _)
+  object LeftFold {
+    def sum(as: List[Int]): Int =
+      foldLeft(as, 0)(_ + _)
 
-  def productlf(as: List[Double]) =
-    foldLeft(as, 1.0)(_ * _)
+    def product(as: List[Double]) =
+      foldLeft(as, 1.0)(_ * _)
 
-  def reverse[A](as: List[A]): List[A] =
-    foldLeft(as, List[A]())((z, x) => x :: z)
+    def append[A](as: List[A], bs: List[A]): List[A] =
+      foldLeft(bs, as)((b, x) => b :+ x)
 
-  def foldLeftFR[A, B](as: List[A], z: B)(f: (B, A) => B): B =
+    def reverse[A](as: List[A]): List[A] =
+      foldLeft(as, List[A]())((z, x) => x :: z)
+
+    def flatten[A](as: List[List[A]]): List[A] =
+      foldLeft(as, Nil: List[A])((z, a) => z ::: foldLeft(a, Nil: List[A])((b, a) => b :+ a))
+  }
+
+  object RightFold {
+    def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B =
+      as match {
+        case Nil     => z
+        case x :: xs => foldLeft(xs, f(z, x))((b, a) => foldRight(List(a), b)((a, b) => f(b, a)))
+      }
+
+    def append[A](as: List[A], bs: List[A]): List[A] =
+      foldRight(as, bs)((x, b) => x :: b)
+
+    def sum(as: List[Int]): Int =
+      foldRight(as, 0)(_ + _)
+
+    def product(as: List[Double]): Double =
+      foldRight(as, 1.0)(_ * _)
+
+    def length[A](as: List[A]): Int =
+      foldRight(as, 0)((_, z) => z + 1)
+  }
+
+  object TailRecursive {
+    def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B =
+      as match {
+        case Nil     => z
+        case x :: xs => foldRight(xs, f(x, z))((a, b) => foldLeft(List(a), b)((a, b) => f(b, a)))
+      }
+  }
+
+  def incByOne(as: List[Int]): List[Int] =
     as match {
-      case Nil     => z
-      case x :: xs => foldLeft(xs, f(z, x))((b, a) => foldRight(List(a), b)((a, b) => f(b, a)))
+      case Nil     => Nil
+      case x :: xs => (x + 1) :: incByOne(xs)
     }
 
-  def foldRightTR[A, B](as: List[A], z: B)(f: (A, B) => B): B =
+  def stringify(as: List[Double]): List[String] =
     as match {
-      case Nil     => z
-      case x :: xs => foldRightTR(xs, f(x, z))((a, b) => foldLeft(List(a), b)((a, b) => f(b, a)))
+      case Nil     => Nil
+      case x :: xs => (x.toString) :: stringify(xs)
     }
 
-  def append1[A](a: A, as: List[A]): List[A] =
+  def map[A, B](as: List[A])(f: A => B): List[B] =
     as match {
-      case Nil     => a :: Nil
-      case x :: xs => x :: append(a, xs)
+      case Nil     => Nil
+      case x :: xs => f(x) :: map(xs)(f)
     }
 
-  def append[A](a: A, as: List[A]): List[A] =
-    foldLeft(as, List[A](a))((b, x) => x :: b)
+  def filter[A](as: List[A])(f: A => Boolean): List[A] =
+    as match {
+      case Nil             => Nil
+      case x :: xs if f(x) => x :: filter(xs)(f)
+      case _ :: xs         => filter(xs)(f)
+    }
+
+  def flatMap[A, B](as: List[A])(f: A => List[B]): List[B] =
+    as match {
+      case Nil     => Nil
+      case x :: xs => f(x) ::: flatMap(xs)(f)
+    }
+
+  object FlatMap {
+    def filter[A](as: List[A])(f: A => Boolean): List[A] =
+      flatMap(as)(a => if (f(a)) List(a) else Nil)
+  }
+
+  def concatAdd(as: List[Int], bs: List[Int]): List[Int] =
+    (as, bs) match {
+      case (a, Nil)           => a
+      case (Nil, b)           => b
+      case (x :: xs, y :: ys) => (x + y) :: concatAdd(xs, ys)
+    }
+
+  def zipWith[A](as: List[A], bs: List[A])(f: (A, A) => A): List[A] =
+    (as, bs) match {
+      case (a, Nil)           => a
+      case (Nil, b)           => b
+      case (x :: xs, y :: ys) => List(f(x, y)) ::: zipWith(xs, ys)(f)
+    }
+
+  def take[A](as: List[A], n: Int): List[A] =
+    as match {
+      case Nil              => Nil
+      case x :: xs if n > 1 => x :: take(xs, n - 1)
+      case x :: _ if n > 0  => List(x)
+      case _ if n <= 0      => Nil
+    }
+
+  def takeWhile[A](as: List[A])(f: A => Boolean): List[A] =
+    as match {
+      case x :: xs if f(x) => x :: takeWhile(xs)(f)
+      case _               => Nil
+    }
+
+  def forall[A](as: List[A])(f: A => Boolean): Boolean =
+    as match {
+      case Nil             => true
+      case x :: xs if f(x) => forall(xs)(f)
+      case _               => false
+    }
+
+  def exists[A](as: List[A], a: A): Boolean =
+    as match {
+      case Nil               => false
+      case x :: xs if x != a => exists(xs, a)
+      case x :: _ if x == a  => true
+    }
+
+  def scanLeft[A, B](as: List[A], z: B)(f: (B, A) => B): List[B] =
+    as match {
+      case Nil     => Nil
+      case x :: xs => f(z, x) :: scanLeft(xs, f(z, x))(f)
+    }
+
+  def scanRight[A, B](as: List[A], z: B)(f: (A, B) => B): List[B] = {
+    val reversed = LeftFold.reverse(as)
+
+    def innerScanRight(as: List[A], z: B)(f: (A, B) => B): List[B] =
+      as match {
+        case Nil     => Nil
+        case x :: xs => innerScanRight(xs, f(x, z))(f) :+ f(x, z)
+      }
+
+    innerScanRight(reversed, z)(f)
+  }
+
+  def hasSubsequence[A](as: List[A], ns: List[A]): Boolean =
+    as match {
+      case Nil => false
+      case _ :: xs =>
+        if (take(as, ns.length) == ns) true
+        else hasSubsequence(xs, ns)
+    }
+
+  sealed trait Tree[+A]
+  case class Leaf[A](value: A)                     extends Tree[A]
+  case class Branch[A](lhs: Tree[A], rhs: Tree[A]) extends Tree[A]
+
+  object Tree {
+    def apply[A](value: A): Tree[A] =
+      new Leaf(value)
+
+    def apply[A](lhs: Tree[A], rhs: Tree[A]): Tree[A] =
+      new Branch(lhs, rhs)
+
+    def size[A](t: Tree[A]): Int =
+      t match {
+        case Leaf(_)          => 1
+        case Branch(lhs, rhs) => size(lhs) + size(rhs) + 1
+      }
+
+    def max(t: Tree[Int]): Int =
+      t match {
+        case Leaf(v)          => v
+        case Branch(lhs, rhs) => max(lhs) max max(rhs)
+      }
+
+    def depth[A](t: Tree[A]): Int =
+      t match {
+        case Leaf(_)          => 1
+        case Branch(lhs, rhs) => (depth(lhs) max depth(rhs)) + 1
+      }
+
+    def map[A, B](t: Tree[A])(f: A => B): Tree[B] =
+      t match {
+        case Leaf(v)          => Leaf(f(v))
+        case Branch(lhs, rhs) => Branch(map(lhs)(f), map(rhs)(f))
+      }
+  }
+
+  object TreeFold {
+    def fold[A, B](t: Tree[A], f: A => B)(g: (B, B) => B): B =
+      t match {
+        case Leaf(v)          => f(v)
+        case Branch(lhs, rhs) => g(fold(lhs, f)(g), fold(rhs, f)(g))
+      }
+
+    def size(t: Tree[Int]): Int =
+      fold[Int, Int](t, _ => 1)(_ + _ + 1)
+
+    def max(t: Tree[Int]): Int =
+      fold[Int, Int](t, a => a)(_ max _)
+
+    def depth(t: Tree[Int]): Int =
+      fold[Int, Int](t, _ => 1)((a, b) => (a max b) + 1)
+
+    def map[A, B](t: Tree[A])(f: A => B): Tree[B] =
+      fold[A, Tree[B]](t, a => Tree(f(a)))(Tree(_, _))
+  }
 }
--- src/test/scala/org/fpinscala/chapters/three/exercisesSpec.scala
+++ src/test/scala/org/fpinscala/chapters/three/exercisesSpec.scala
@@ -1,9 +1,10 @@
 package org.fpinscala.chapters.three
 
 import org.fpinscala.chapters.three.Exercises._
-import org.scalatest.{FlatSpec, Matchers}
+import org.fpinscala.chapters.three.Exercises.Tree._
+import org.scalatest.{FlatSpec, Matchers, Inspectors}
 
-class ExercisesSpec extends FlatSpec with Matchers {
+class ExercisesSpec extends FlatSpec with Matchers with Inspectors {
 
   behavior of "exercise 3.2"
 
@@ -101,10 +102,14 @@ class ExercisesSpec extends FlatSpec with Matchers {
   behavior of "exercise 3.7 - sum"
 
   it should "returns 0 when empty list" in {
+    import org.fpinscala.chapters.three.Exercises.RightFold._
+
     sum(Nil) shouldBe 0
   }
 
   it should "return the total sum of all list elements" in {
+    import org.fpinscala.chapters.three.Exercises.RightFold._
+
     val as = 1 :: 2 :: 3 :: Nil
 
     sum(as) shouldBe 6
@@ -113,10 +118,14 @@ class ExercisesSpec extends FlatSpec with Matchers {
   behavior of "exercise 3.7 - product"
 
   it should "return 1.0 when empty list" in {
+    import org.fpinscala.chapters.three.Exercises.RightFold._
+
     product(Nil) shouldBe 1.0
   }
 
   it should "return the total product of all list elements" in {
+    import org.fpinscala.chapters.three.Exercises.RightFold._
+
     val as = 1.0 :: 2.0 :: 3.0 :: Nil
 
     product(as) shouldBe 6.0
@@ -127,13 +136,13 @@ class ExercisesSpec extends FlatSpec with Matchers {
   it should "return 0 for the empty list" in {
     val as = Nil
 
-    Exercises.length(as) shouldBe 0
+    RightFold.length(as) shouldBe 0
   }
 
   it should "return the total length of the list" in {
     val as = 1 :: 2 :: 3 :: Nil
 
-    Exercises.length(as) shouldBe 3
+    RightFold.length(as) shouldBe 3
   }
 
   behavior of "exercise 3.10"
@@ -153,34 +162,46 @@ class ExercisesSpec extends FlatSpec with Matchers {
   behavior of "exercise 3.11 - sumlf"
 
   it should "return 0 when empty list" in {
-    sumlf(Nil) shouldBe 0
+    import org.fpinscala.chapters.three.Exercises.LeftFold._
+
+    sum(Nil) shouldBe 0
   }
 
   it should "return the total sum of all list elements" in {
+    import org.fpinscala.chapters.three.Exercises.LeftFold._
+
     val as = 1 :: 2 :: 3 :: Nil
 
-    sumlf(as) shouldBe 6
+    sum(as) shouldBe 6
   }
 
   behavior of "exercise 3.11 - productlf"
 
   it should "return 1.0 when empty list" in {
-    productlf(Nil) shouldBe 1.0
+    import org.fpinscala.chapters.three.Exercises.LeftFold._
+
+    product(Nil) shouldBe 1.0
   }
 
   it should "return the total product of all list elements" in {
+    import org.fpinscala.chapters.three.Exercises.LeftFold._
+
     val as = 1.0 :: 2.0 :: 3.0 :: Nil
 
-    productlf(as) shouldBe 6.0
+    product(as) shouldBe 6.0
   }
 
   behavior of "exercise 3.12"
 
   it should "return 'Nil' on empty list" in {
+    import org.fpinscala.chapters.three.Exercises.LeftFold._
+
     reverse(Nil) shouldBe Nil
   }
 
   it should "return a list with all elements in reverse order" in {
+    import org.fpinscala.chapters.three.Exercises.LeftFold._
+
     val as = 1 :: 2 :: 3 :: Nil
 
     reverse(as) shouldBe 3 :: 2 :: 1 :: Nil
@@ -189,43 +210,478 @@ class ExercisesSpec extends FlatSpec with Matchers {
   behavior of "exercise 3.12"
 
   it should "return init accumulated value when empty List" in {
+    import org.fpinscala.chapters.three.Exercises.RightFold._
+
     val as: List[Int] = Nil
 
-    foldLeftFR(as, 0)(_ + _) shouldBe 0
+    foldLeft(as, 0)(_ + _) shouldBe 0
   }
 
   it should "return the accumulated value applying the binary operation on all elements" in {
+    import org.fpinscala.chapters.three.Exercises.RightFold._
+
     val as: List[Int] = 1 :: 2 :: 3 :: Nil
     val z: List[Int]  = Nil
 
-    foldLeftFR(as, z)((z, a) => a :: z) shouldBe 3 :: 2 :: 1 :: Nil
+    foldLeft(as, z)((z, a) => a :: z) shouldBe 3 :: 2 :: 1 :: Nil
   }
 
   behavior of "exercise 3.13"
 
   it should "return init accumulated value when empty List" in {
+    import org.fpinscala.chapters.three.Exercises.TailRecursive._
+
     val as: List[Int] = Nil
 
-    foldRightTR(as, 0)(_ + _) shouldBe 0
+    foldRight(as, 0)(_ + _) shouldBe 0
   }
 
   it should "return the accumulated value applying the binary operation on all elements" in {
+    import org.fpinscala.chapters.three.Exercises.TailRecursive._
+
     val as = 1 :: 2 :: 3 :: Nil
 
-    foldRightTR(as, List[Int]())((a: Int, z: List[Int]) => a :: z) shouldBe 3 :: 2 :: 1 :: Nil
+    foldRight(as, List[Int]())((a: Int, z: List[Int]) => a :: z) shouldBe 3 :: 2 :: 1 :: Nil
   }
 
   behavior of "exercise 3.14"
 
-  it should "return a single element list on appending to empty list" in {
+  it should "return a single element list on appending to empty list by foldLeft" in {
+    import org.fpinscala.chapters.three.Exercises.LeftFold._
+
     val as: List[Int] = Nil
 
-    append(1, as) shouldBe 1 :: Nil
+    append(List(1), as) shouldBe 1 :: Nil
   }
 
-  it should "return a new list with the new element on last position" in {
+  it should "return a new list with the new element on last position by foldLeft" in {
+    import org.fpinscala.chapters.three.Exercises.LeftFold._
+
+    val as = 1 :: 2 :: 3 :: Nil
+    val bs = 4 :: 5 :: 6 :: Nil
+
+    append(as, bs) shouldBe as ::: bs
+  }
+
+  it should "return a single element list on appending to empty list by foldRight" in {
+    import org.fpinscala.chapters.three.Exercises.RightFold._
+
+    val as: List[Int] = Nil
+
+    append(List(1), as) shouldBe 1 :: Nil
+  }
+
+  it should "return a new list with the new element on last position by foldRight" in {
+    import org.fpinscala.chapters.three.Exercises.RightFold._
+
+    val as = 1 :: 2 :: 3 :: Nil
+    val bs = 4 :: 5 :: 6 :: Nil
+
+    append(as, bs) shouldBe as ::: bs
+  }
+
+  behavior of "exercise 3.15"
+
+  it should "return Nil on empty list" in {
+    import org.fpinscala.chapters.three.Exercises.LeftFold._
+
+    val as: List[List[Int]] = Nil
+
+    flatten(as) shouldBe Nil
+  }
+
+  it should "return a simple list with all elements from any sublist" in {
+    import org.fpinscala.chapters.three.Exercises.LeftFold._
+
+    val as: List[List[Int]] = List(List(1, 2, 3), List(4, 5, 6))
+    val expected            = 1 :: 2 :: 3 :: 4 :: 5 :: 6 :: Nil
+
+    flatten(as) shouldBe expected
+  }
+
+  behavior of "exercise 3.16"
+
+  it should "return Nil when empty list" in {
+    incByOne(Nil: List[Int]) shouldBe Nil
+  }
+
+  it should "return a new list with incremented elements" in {
+    val as = 1 :: 2 :: 3 :: 4 :: Nil
+
+    incByOne(as) shouldBe 2 :: 3 :: 4 :: 5 :: Nil
+  }
+
+  behavior of "exercise 3.17"
+
+  it should "return Nil when empty list" in {
+    stringify(Nil: List[Double]) shouldBe Nil
+  }
+
+  it should "return a list with stringified doubles" in {
+    val as = 1.0 :: 2.0 :: 3.0 :: Nil
+
+    stringify(as) shouldBe "1.0" :: "2.0" :: "3.0" :: Nil
+  }
+
+  behavior of "exercise 3.18"
+
+  it should "return Nil when empty list" in {
+    Exercises.map(Nil: List[Int])(a => a) shouldBe Nil
+  }
+
+  it should "return a list with mapped element by given op" in {
     val as = 1 :: 2 :: 3 :: Nil
 
-    append(4, as) shouldBe 1 :: 2 :: 3 :: 4 :: Nil
+    Exercises.map(as)(_ + 1) shouldBe 2 :: 3 :: 4 :: Nil
+  }
+
+  behavior of "exercise 3.19"
+
+  it should "return Nil when empty list" in {
+    filter(Nil: List[Int])(BigInt(_).mod(2) == 0) shouldBe Nil
+  }
+
+  it should "return list with even numbers" in {
+    val as = 1 :: 2 :: 3 :: 4 :: Nil
+
+    filter(as)(BigInt(_).mod(2) == 0) shouldBe 2 :: 4 :: Nil
+  }
+
+  behavior of "exercise 3.20"
+
+  it should "return Nil when empty list" in {
+    flatMap(Nil: List[Int])(a => List(a)) shouldBe Nil
+  }
+
+  it should "should return a list with doubled elements" in {
+    val as = 1 :: 2 :: 3 :: Nil
+
+    flatMap(as)(a => List(a, a)) shouldBe List(1, 1, 2, 2, 3, 3)
+  }
+
+  behavior of "exercise 3.21"
+
+  it should "return Nil when empty list" in {
+    import org.fpinscala.chapters.three.Exercises.FlatMap._
+
+    filter(Nil: List[Int])(BigInt(_).mod(2) == 0) shouldBe Nil
+  }
+
+  it should "return list with even numbers" in {
+    import org.fpinscala.chapters.three.Exercises.FlatMap._
+
+    val as = 1 :: 2 :: 3 :: 4 :: Nil
+
+    filter(as)(BigInt(_).mod(2) == 0) shouldBe 2 :: 4 :: Nil
+  }
+
+  behavior of "exercise 3.22"
+
+  it should "return the non empty input list when the other is Nil" in {
+    val as            = 1 :: 2 :: 3 :: Nil
+    val bs: List[Int] = Nil
+
+    concatAdd(as, bs) shouldBe as
+    concatAdd(bs, as) shouldBe as
+  }
+
+  it should "return a new list with th sum of each element pair by position" in {
+    val as = 1 :: 2 :: 3 :: Nil
+    val bs = 4 :: 5 :: 6 :: Nil
+
+    concatAdd(as, bs) shouldBe 5 :: 7 :: 9 :: Nil
+  }
+
+  behavior of "exercise 3.23"
+
+  it should "return then non empty input list when the other is Nil" in {
+    val as            = 1 :: 2 :: 3 :: Nil
+    val bs: List[Int] = Nil
+    val f             = (_: Int, _: Int) => 1
+
+    zipWith(as, bs)(f) shouldBe as
+    zipWith(bs, as)(f) shouldBe as
+  }
+
+  it should "return the zipped list by applying the binary operation on each element pair" in {
+    val as = 1 :: 2 :: 3 :: Nil
+    val bs = 4 :: 5 :: 6 :: Nil
+
+    zipWith(as, bs)(_ + _) shouldBe 5 :: 7 :: 9 :: Nil
+  }
+
+  behavior of "take n"
+
+  it should "return Nil on empty list" in {
+    take(Nil: List[Int], 3) shouldBe Nil
+  }
+
+  it should "return a new list with first n elements of init list" in {
+    val as = 1 :: 2 :: 3 :: 4 :: 5 :: 6 :: Nil
+
+    take(as, 2) shouldBe 1 :: 2 :: Nil
+  }
+
+  it should "return Nil if taking zero elements of init list" in {
+    val as = 1 :: 2 :: 3 :: 4 :: 5 :: 6 :: Nil
+
+    take(as, 0) shouldBe Nil
+  }
+
+  it should "return Nil if taking negative amount of elements of init list" in {
+    val as = 1 :: 2 :: 3 :: 4 :: 5 :: 6 :: Nil
+
+    take(as, -1) shouldBe Nil
+  }
+
+  behavior of "takeWhile"
+
+  it should "return Nil on empty list" in {
+    val f = (_: Int) => false
+
+    takeWhile(Nil: List[Int])(f) shouldBe Nil
+  }
+
+  it should "return a new list of elements until odd number check is true" in {
+    val as = 1 :: 2 :: 3 :: 4 :: 5 :: 6 :: Nil
+    val f  = (a: Int) => BigInt(a).mod(2) != 0
+
+    takeWhile(as)(f) shouldBe 1 :: Nil
+  }
+
+  behavior of "forall"
+
+  it should "return true if empty list" in {
+    val f = (_: Int) => false
+
+    forall(Nil: List[Int])(f) shouldBe true
+  }
+
+  it should "return true if all elements are even numbers" in {
+    val as = 2 :: 4 :: 6 :: 8 :: 10 :: Nil
+    val f  = (a: Int) => BigInt(a).mod(2) == 0
+
+    forall(as)(f) shouldBe true
+  }
+
+  it should "return false if on element is not an even number" in {
+    val as = 1 :: 2 :: 4 :: 6 :: 8 :: 10 :: Nil
+    val f  = (a: Int) => BigInt(a).mod(2) == 0
+
+    forall(as)(f) shouldBe false
+  }
+
+  behavior of "exists"
+
+  it should "return false when empty list" in {
+    exists(Nil: List[Int], 2) shouldBe false
+  }
+
+  it should "return false when element does not exists in list" in {
+    val as = 1 :: 2 :: 4 :: 6 :: 8 :: 10 :: Nil
+
+    exists(as, 3) shouldBe false
+  }
+
+  it should "return true when element exists in list" in {
+    val as = 1 :: 2 :: 4 :: 6 :: 8 :: 10 :: Nil
+
+    exists(as, 6) shouldBe true
+  }
+
+  behavior of "scanLeft"
+
+  it should "return Nil when empty list" in {
+    scanLeft(Nil: List[Int], 1)((_, _) => 1) shouldBe Nil
+  }
+
+  it should "return the list of partial results by applying the binary operation" in {
+    val as = 1 :: 2 :: 3 :: Nil
+
+    scanLeft(as, 0)(_ + _) shouldBe 1 :: 3 :: 6 :: Nil
+  }
+
+  behavior of "scanRight"
+
+  it should "return Nil when empty list" in {
+    scanRight(Nil: List[Int], 1)((_, _) => 1) shouldBe Nil
+  }
+
+  it should "return the list of partial results by applying the binary operation" in {
+    val as = 1 :: 2 :: 3 :: Nil
+
+    scanRight(as, 0)(_ + _) shouldBe 6 :: 5 :: 3 :: Nil
+  }
+
+  behavior of "exercise 3.24"
+
+  it should "return true when needle is the empty list" in {
+    val as = 1 :: 2 :: 3 :: Nil
+
+    hasSubsequence(as, Nil: List[Int]) shouldBe true
+  }
+
+  it should "return true if needle is a sublist of list" in {
+    val as = 1 :: 2 :: 3 :: Nil
+    val ns = 2 :: Nil
+
+    hasSubsequence(as, ns) shouldBe true
+  }
+
+  it should "return false if needle is not a sublist of list" in {
+    val as = 1 :: 2 :: 3 :: Nil
+    val ns = 4 :: Nil
+
+    hasSubsequence(as, ns) shouldBe false
+  }
+
+  it should "return true if needles is a sublist of list" in {
+    val as = 1 :: 2 :: 3 :: 4 :: Nil
+
+    val ns1 = 1 :: 2 :: Nil
+    val ns2 = 2 :: 3 :: Nil
+    val ns3 = 3 :: 4 :: Nil
+
+    forAll(Seq(ns1, ns2, ns3))((ns) => hasSubsequence(as, ns) shouldBe true)
+  }
+
+  behavior of "exercise 3.25"
+
+  it should "return 1 when root" in {
+    val tree = Tree(1)
+
+    Tree.size(tree) shouldBe 1
+  }
+
+  it should "return the amount of nodes of the tree" in {
+    val tree = Tree(Tree(1), Tree(Tree(2), Tree(3)))
+
+    Tree.size(tree) shouldBe 5
+  }
+
+  behavior of "exercise 3.26"
+
+  it should "return the root element when single element tree" in {
+    val tree = Tree(1)
+
+    max(tree) shouldBe 1
+  }
+
+  it should "return the maxium of all nodes of the tree" in {
+    val tree = Tree(Tree(1), Tree(Tree(5), Tree(3)))
+
+    max(tree) shouldBe 5
+  }
+
+  behavior of "exercise 3.27"
+
+  it should "return 1 when single element tree" in {
+    val tree = Tree(1)
+
+    depth(tree) shouldBe 1
+  }
+
+  it should "return the maximum depth of a tree" in {
+    val tree: Tree[Int] = Tree(
+      Tree(
+        Tree(1),
+        Tree(2)
+      ),
+      Tree(
+        Tree(
+          Tree(4),
+          Tree(5)
+        ),
+        Tree(3)
+      )
+    )
+
+    depth(tree) shouldBe 4
+  }
+
+  behavior of "exercise 3.28"
+
+  it should "return the single mapped tree" in {
+    val tree: Tree[Int] = Tree(1)
+    val f               = (value: Int) => value + 1
+
+    Tree.map(tree)(f) shouldBe Tree(2)
+  }
+
+  it should "return the mapped tree by applying f on each leaf" in {
+    val tree: Tree[Int] = Tree(Tree(Tree(1), Tree(2)), Tree(Tree(3), Tree(4)))
+
+    val expected: Tree[Int] = Tree(Tree(Tree(2), Tree(3)), Tree(Tree(4), Tree(5)))
+
+    val f = (value: Int) => value + 1
+
+    Tree.map(tree)(f) shouldBe expected
+  }
+
+  behavior of "exercise 3.29"
+
+  it should "return 1 when root" in {
+    val tree = Tree(1)
+
+    TreeFold.size(tree) shouldBe 1
+  }
+
+  it should "return the amount of nodes of the tree" in {
+    val tree = Tree(Tree(1), Tree(Tree(2), Tree(3)))
+
+    TreeFold.size(tree) shouldBe 5
+  }
+
+  it should "return the root element when single element tree" in {
+    val tree = Tree(1)
+
+    TreeFold.max(tree) shouldBe 1
+  }
+
+  it should "return the maxium of all nodes of the tree" in {
+    val tree = Tree(Tree(1), Tree(Tree(5), Tree(3)))
+
+    TreeFold.max(tree) shouldBe 5
+  }
+
+  it should "return 1 when single element tree" in {
+    val tree = Tree(1)
+
+    TreeFold.depth(tree) shouldBe 1
+  }
+
+  it should "return the maximum depth of a tree" in {
+    val tree: Tree[Int] = Tree(
+      Tree(
+        Tree(1),
+        Tree(2)
+      ),
+      Tree(
+        Tree(
+          Tree(4),
+          Tree(5)
+        ),
+        Tree(3)
+      )
+    )
+
+    TreeFold.depth(tree) shouldBe 4
+  }
+
+  it should "return the single mapped tree" in {
+    val tree: Tree[Int] = Tree(1)
+    val f               = (value: Int) => value + 1
+
+    TreeFold.map(tree)(f) shouldBe Tree(2)
+  }
+
+  it should "return the mapped tree by applying f on each leaf" in {
+    val tree: Tree[Int] = Tree(Tree(Tree(1), Tree(2)), Tree(Tree(3), Tree(4)))
+
+    val expected: Tree[Int] = Tree(Tree(Tree(2), Tree(3)), Tree(Tree(4), Tree(5)))
+
+    val f = (value: Int) => value + 1
+
+    TreeFold.map(tree)(f) shouldBe expected
   }
 }
