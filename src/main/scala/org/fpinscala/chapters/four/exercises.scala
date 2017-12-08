2b6372cd4c550a9ced5b3c503a40c5c2af13a829
Periklis Tsirakidis <periklis.tsirakidis@holidaycheck.com>
2017-12-08 18:21:04 +0100

    Implement solutions and tests for exercise 4.1

--- src/main/scala/org/fpinscala/chapters/four/exercises.scala
+++ src/main/scala/org/fpinscala/chapters/four/exercises.scala
@@ -1,3 +1,41 @@
 package org.fpinscala.chapters.four
 
-object Exercises {}
+object Exercises {
+
+  sealed trait Option[+A]
+  case object None            extends Option[Nothing]
+  case class Some[+A](get: A) extends Option[A]
+
+  object Option {
+    def map[A, B](ob: Option[A])(f: A => B): Option[B] =
+      ob match {
+        case None    => None
+        case Some(v) => Some(f(v))
+      }
+
+    def flatMap[A, B](ob: Option[A])(f: A => Option[B]): Option[B] =
+      ob match {
+        case None    => None
+        case Some(v) => f(v)
+      }
+
+    def getOrElse[A, B >: A](ob: Option[A])(default: => B): B =
+      ob match {
+        case None    => default
+        case Some(v) => v
+      }
+
+    def orElse[A, B >: A](ob: Option[A])(oe: => Option[B]): Option[B] =
+      ob match {
+        case None    => oe
+        case Some(v) => Some(v)
+      }
+
+    def filter[A](ob: Option[A])(f: A => Boolean): Option[A] =
+      ob match {
+        case None            => None
+        case Some(v) if f(v) => Some(v)
+        case _               => None
+      }
+  }
+}
--- src/test/scala/org/fpinscala/chapters/four/exercisesSpec.scala
+++ src/test/scala/org/fpinscala/chapters/four/exercisesSpec.scala
@@ -1,11 +1,69 @@
 package org.fpinscala.chapters.four
 
 import org.fpinscala.chapters.four.Exercises._
+import org.fpinscala.chapters.four.Exercises.Option._
 import org.scalatest.{FlatSpec, Matchers}
 
 class ExercisesSpec extends FlatSpec with Matchers {
 
-  behavior of "4.1"
+  behavior of "exercise 4.1 - Option"
 
-  it should "return true" in {}
+  it should "return map to None if Option None" in {
+    val ob: Option[Int] = None
+    map(ob)(_ + 1) shouldBe None
+  }
+
+  it should "return mapped option for Some value" in {
+    val ob = Some(1)
+    map(ob)(_ + 1) shouldBe Some(2)
+  }
+
+  it should "return None when flatMap on Option None" in {
+    val ob: Option[Int] = None
+
+    flatMap(ob)(a => Some(a + 1)) shouldBe None
+  }
+
+  it should "return option of mapped value for Some value" in {
+    val ob = Some(1)
+    flatMap(ob)(a => Some(a.toString)) shouldBe Some("1")
+  }
+
+  it should "return default value when option None" in {
+    val ob: Option[Int] = None
+
+    getOrElse(ob)(2) shouldBe 2
+  }
+
+  it should "return initial value when option of some value" in {
+    val ob = Some(1)
+
+    getOrElse(ob)(2) shouldBe 1
+  }
+
+  it should "return other option when option None" in {
+    val ob: Option[Int] = None
+    val oe              = Some(1)
+
+    orElse(ob)(oe) shouldBe oe
+  }
+
+  it should "return intial option when some option value" in {
+    val ob = Some(1)
+    val oe = Some(2)
+
+    orElse(ob)(oe) shouldBe ob
+  }
+
+  it should "return None when option None" in {
+    val ob: Option[Int] = None
+
+    filter(ob)(a => true) shouldBe None
+  }
+
+  it should "return the option value when filter applies" in {
+    val ob = Some(1)
+
+    filter(ob)(BigInt(_).mod(2) != 0) shouldBe ob
+  }
 }
