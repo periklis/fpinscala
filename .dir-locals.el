0dc8b29307c1eead98408712964e677c401d6c9c
Periklis Tsirakidis <periklis.tsirakidis@holidaycheck.com>
2018-03-10 16:29:08 +0100

    Tell ensime to perform less work

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,7 @@
-((scala-mode
+((nil
   .
-  ((scala-indent:align-parameters . t))))
+  ((ensime-sbt-perform-on-save "compile")))
+ (scala-mode
+  .
+  ((scala-indent:align-parameters . t)
+   (ensime-eldoc-hints . nil))))
