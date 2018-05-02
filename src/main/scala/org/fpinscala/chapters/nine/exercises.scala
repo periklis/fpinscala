5778916854de77dbe05a346095878e65c829af61
Periklis Tsirakidis <periklis.tsirakidis@holidaycheck.com>
2018-05-02 18:16:46 +0200

    Init chapter 9 exercises

--- src/main/scala/org/fpinscala/chapters/nine/exercises.scala
+++ src/main/scala/org/fpinscala/chapters/nine/exercises.scala
@@ -1,11 +1,15 @@
 package org.fpinscala.chapters.nine
 
+import org.fpinscala.chapters.eight.Exercises.Prop.forAll
+import org.fpinscala.chapters.eight.Exercises.{Gen, Prop}
+
 trait Parsers[ParseError, Parser[+ _]] { self =>
 
   def run[A](p: Parser[A])(input: String): Either[ParseError, A]
 
   // Law: run(char(c))(c.toString) === Right(c)
-  def char[A](c: Char): Parser[A]
+  def char[A](c: Char): Parser[Char] =
+    string(c.toString).map(_.charAt(0))
 
   // Law: run(string(s))(s) === Right(s)
   def string(s: String): Parser[String]
@@ -14,13 +18,11 @@ trait Parsers[ParseError, Parser[+ _]] { self =>
 
   def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]]
 
-  // Law run(zeroOrMore(char(c)))(c.toString) === Right(1)
-  // Law run(zeroOrMore(listOfN(2, char(c))))(s"${c.toString}${c.toString}") === Right(2)
-  def zeroOrMore[A](p: Parser[A]): Parser[Int]
+  def many[A](p: Parser[A]): Parser[List[A]]
 
-  // Law run(atLeastOne(char(c)))(c.toString) === Right(2)
-  // Law run(atLeastOne(char(c)))(b.toString) === Left("Expected a least one: c")
-  def atLeastOne[A](p: Parser[A]): Parser[Int]
+  def map[A, B](f: A => B): Parser[B]
+
+  def slice[A](p: Parser[A]): Parser[String]
 
   implicit def fromString(s: String): Parser[String] = string(s)
   implicit def operators[A](p: Parser[A])            = ParserOps(p)
@@ -28,9 +30,17 @@ trait Parsers[ParseError, Parser[+ _]] { self =>
     ParserOps(f(a))
 
   case class ParserOps[A](p: Parser[A]) {
-    def *(): Parser[Int] = self.zeroOrMore(p)
-    def +(): Parser[Int] = self.atLeastOne(p)
-    def |[B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
+    def |[B >: A](p2: Parser[B]): Parser[B]  = self.or(p, p2)
     def or[B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
+    def many(): Parser[List[A]]              = self.many(p)
+    def map[B](f: A => B): Parser[B]         = p.map(f)
+  }
+
+  object Laws {
+    def equal[A](p1: Parser[A], p2: Parser[A])(in: Gen[String]): Prop =
+      forAll(in)(s => run(p1)(s) == run(p2)(s))
+
+    def mapLaw[A](p: Parser[A])(in: Gen[String]): Prop =
+      equal(p, p.map(a => a))(in)
   }
 }
